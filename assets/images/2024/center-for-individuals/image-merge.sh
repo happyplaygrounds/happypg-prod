93be4dee577b3649baba1cc371bb03fddb713e85
Brian Collins <brian.collins10s@gmail.com>
2024-03-13 11:26:25 -0500

    center blog post

---
+++ assets/images/2024/center-for-individuals/image-merge.sh
@@ -0,0 +1,6 @@
+#convert ...read_images... \
+                #-background white -splice 10x0+0+0  +append -chop 10x0+0+0 \
+               #result.png
+
+#convert +append IMG_0363.jpeg IMG_0381.jpeg riverwalk-3.jpg
+convert center-15.jpg center-16.jpg -background white -splice 10x0+0+0  +append -chop 10x0+0+0 center-17.jpg
